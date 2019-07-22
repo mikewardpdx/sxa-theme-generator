a70a217f5a6e09409dd172207aa9e38841a36447
Mike Ward <mike.ward@perficient.com>
2019-07-22 13:18:47 -0700

    fix(): correct storbook config to display stories

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,7 +1,7 @@
 import { configure, addDecorator } from '@storybook/vue';
 import { withA11y } from '@storybook/addon-a11y';
 
-const req = require.context('../scripts', true, /\.stories\.t(s|sx)$/);
+const req = require.context('../scripts/', true, /\.stories\.t(s|sx)$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
